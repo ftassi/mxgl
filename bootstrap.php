d1a624b83f78fa047e797db9f4956639e8073cc2
Francesco Tassi <tassi.francesco@gmail.com>
2011-11-15 22:15:23 +0100

    index. TEST

--- bootstrap.php
+++ bootstrap.php
@@ -5,20 +5,27 @@ ini_set('display_errors', 1);
 require_once __DIR__ . '/silex.phar';
 require_once __DIR__ . '/vendor/simplehtmldom/simple_html_dom.php';
 
-use Symfony\Component\Yaml\Yaml;
 
 $app = new Silex\Application();
-$app['autoloader']->registerNamespace("Symfony\Component\Yaml", __DIR__ . "/vendor");
-$app['config'] = Yaml::parse(__DIR__ . "/config/config.yml");
 
-$app['debug'] = $app['config']['app']['debug'];
+$app['debug'] = true;
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => $app['config']['db'],
+    'db.options' => array(
+        'driver' => 'pdo_mysql',
+        'dbname' => 'mxgl',
+        'user' => 'root',
+        'password' => 'root',
+        'host' => 'localhost',
+        
+    ),
     'db.dbal.class_path' => __DIR__ . '/vendor/doctrine-dbal/lib',
     'db.common.class_path' => __DIR__ . '/vendor/doctrine-common/lib',
 ));
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/view',
     'twig.class_path' => __DIR__ . '/vendor/twig/lib',
 ));
+
+echo $_ENV['dbname'];
