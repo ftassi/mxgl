bfab0d5aeb80894b37af1d2d41ee1a13c655347c
Francesco Tassi <tassi.francesco@gmail.com>
2011-11-15 23:49:27 +0100

    installata estensione silex_doctrine_mongodb_extension.phar e dipendenze

--- silex/bootstrap.php
+++ silex/bootstrap.php
@@ -4,31 +4,31 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once __DIR__ . '/silex.phar';
-require_once __DIR__ . '/extension/MongoExtension/MongoExtension.php';
+require_once __DIR__ . '/silex_doctrine_mongodb_extension.phar';
 require_once __DIR__ . '/vendor/simplehtmldom/simple_html_dom.php';
 
+use Silex\Extension\DoctrineMongoDBExtension;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
 $app = new Silex\Application();
-
 $app['debug'] = true;
-
-$app->register(new MongoExtension(), array(
-    'mongo.options' => array(
-        'dbname' => 'madieru',
-        'server' => 'localhost',
-    ),
-    'mongo.common.class_path' => __DIR__ . '/vendor/doctrineCommon/lib',
-    'mongo.mongodb.class_path' => __DIR__ . '/vendor/doctrineMongoDB/lib',
-    'mongo.mongodbodm.class_path' => __DIR__ . '/vendor/doctrineMongoDBODM/lib',
-    'mongo.common.proxy_dir' => __DIR__ . '/cache',
-    'mongo.common.hydrator_dir' => __DIR__ . '/cache',
-    'mongo.common.documents_dir' => __DIR__ . '/lib',
-));
-
-
-// if you want to autoload your documents, use the autoloader service:
 $app['autoloader']->registerNamespace('Document', __DIR__);
 
+$app->register(new DoctrineMongoDBExtension, array(
+    'doctrine.odm.mongodb.connection_options' => array(
+        'database' => 'bananas',
+        'host' => 'localhost',
+    ),
+    'doctrine.odm.mongodb.documents' => array(
+        array('type' => 'annotation', 'path' => __DIR__ . '/Document', 'namespace' => 'Document'),
+    ),
+    'doctrine.common.class_path' => __DIR__ . '/vendor/doctrine-common/lib',
+    'doctrine.mongodb.class_path' => __DIR__ . '/vendor/doctrine-mongodb/lib',
+    'doctrine.odm.mongodb.class_path' => __DIR__ . '/vendor/doctrine-mongodb-odm/lib',
+));
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/view',
     'twig.class_path' => __DIR__ . '/vendor/twig/lib',
 ));
+
+AnnotationRegistry::registerFile(__DIR__.'/vendor/doctrine-mongodb-odm/lib/Doctrine/ODM/MongoDB/Mapping/Annotations/DoctrineAnnotations.php');
