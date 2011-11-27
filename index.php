81719e3e684007d03043b0ad1a3a293d60f94b54
Francesco Tassi <tassi.francesco@gmail.com>
2011-11-27 15:56:13 +0100

    implementazione grezza lista regali e form di aggiunta non funzionante

--- index.php
+++ index.php
@@ -6,35 +6,29 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $app->match('/', function () use ($app)
-    {
-        if ($app['facebook']->getUser() > 0) {
-            return $app->redirect('/add-gift');
-        }
+        {
+            if ($app['facebook']->getUser() > 0) {
+                return $app->redirect('/add-gift');
+            }
 
-        return $app['twig']->render('welcome.twig', array(
-                'facebook' => $app['facebook'],
-            ));
-    })
+            return $app['twig']->render('welcome.twig', array(
+                    'facebook' => $app['facebook'],
+                ));
+        })
     ->method('GET|POST');
 
-$app->post('/add-gift', function(Request $request) use($app)
-    {
-        $formData = $request->get('gift');
-        /* @var $gift Document\Gift */
-        $giftDocument = $app['gift'];
-        $giftDocument->fromHtmlDom(file_get_html($formData['url']));
-        $giftDocument->setUserId($formData['user_id']);
+$app->match('/add-gift', function(Request $request) use($app)
+        {
+            $userGiftsList = $app['doctrine.odm.mongodb.dm']
+                ->getRepository('Document\Gift')
+                ->findBy(array('userId' => $app['facebook']->getUser()));
 
-        $app['doctrine.odm.mongodb.dm']->persist($giftDocument);
-        $app['doctrine.odm.mongodb.dm']->flush();
+            return $app['twig']->render('my_gift_list.twig', array(
+                    'facebook' => $app['facebook'],
+                    'gifts_list' => $userGiftsList,
+                ));
+        })
+    ->method('GET|POST');
 
-        $userGiftsList = $app['doctrine.odm.mongodb.dm']
-            ->getRepository('Document\Gift')
-            ->findBy(array('user_id' => $formData['user_id']));
-        
-        return $app['twig']->render('index.twig', array(
-                'user_id' => $formData['user_id'],
-                'gifts_list' => $userGiftsList,
-            ));
-    });
+
 $app->run();
